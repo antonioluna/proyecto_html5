a9cd8462c45ff0f28f6fecfce05fc69dd87d22e0
Antonio Luna <antonio__luna@outlook.es>
2015-03-01 22:59:58 +0100

    añado un script que introduce frases aleatorias

---
+++ js/frases.js
@@ -0,0 +1,52 @@
+// Completamos el vector con frases ,el número de casillas es indiferente
+var Quotation=new Array()
+Quotation[0] = "Cuando la radio le regala un elefante a Bart y éstos se escapan flanders exclama: 'los cuatro elefantes del apocalipsis' mientras su mujer le corrije diciéndole 'los cuatro jinetes'. Este chiste pierde al ser traducido, en inglés los cuatro jinetes se les llama 'los cuatro fantasmas' (The four phantom) que es una palabra que se asemeja a elefante (elephant)."; 
+Quotation[1] = "Los seguidores de la serie, como el propio productor David Silverman, están de acuerdo en que Waylon Smithers es homosexual, o al menos bisexual"; 
+Quotation[2] = "Hay discrepancias sobre la edad de Homer, aunque las cifras barajadas son muy similares: 35, 36 (la más aceptada) o 38 años"; 
+Quotation[3] = "La edad del señor Burns es sin duda de «más de cien años». En el episodio «Who Shot Mr. Burns?» («¿Quién disparó al señor Burns?») la cifra que se da es de 104 años, mientras que en «Simpson and Delilah» («Simpson y Dalila») son 81."; 
+Quotation[4] = "En un episodio, a Lisa le cambian a Tercer Grado y a Bart le degradan a Tercero también"; 
+Quotation[5] = "«Los Simpson» han recibido decenas de nominaciones a los premios Emmy, además de otros premios incluyendo seis premios Annie consecutivos y el premio Peabody en 1997"; 
+Quotation[6] = "En el episodio de la temporada 16 Homer-Móvil (Mobile Homer), Lisa encuentra un mapa de Los Picapiedra en el que hay una anotación sobre Dino, y concluye Homer 'Preguntarle a Jeeves'; en la versión doblada para España, sustituyen esa frase por otra que dice: «consultarlo en Wikipedia»"; 
+Quotation[7] = "Los dos amigos de Homer se llaman Carl y Lenni, quienes guardan cierta similitud con Karl Marx y Vladimir Lenin"; 
+Quotation[8] = "El pelo de la nuca de Homer y la oreja, forman dos letras, una M y una G, que son las iniciales de Matt Groening"; 
+Quotation[9] = "La dirección de los Simpson varía entre episodios, aunque la más habitual es Evergreen Terrace, 742, inspirada en la calle donde Matt Groening vivió de pequeño. Básicamente la dirección se mantiene constante o con pequeñas variaciones (en el número de la calle principalmente), excepto en el episodio «Kamp Krusty“, donde la dirección de los Simpson es Spalding Way, 430, Springfield, presumiblemente en honor al monologuista y actor Spalding Gray cuyo humor también se considera sutil, agudo y en general irritante para los republicanos"; 
+Quotation[10] = "El número de teléfono de los Simpson también puede variar entre episodios. Según la agenda del director Skinner, de la Escuela Primaria de Springfield, los teléfonos de Homer han sido: 555–6528 (casa) y 555–7334 (trabajo); 555–6832 (casa) y 555–6754 (trabajo). En la Tercera temporada era el 555–8707 y en el episodio de debut televisivo «Mr. Plow» fue el 555–3223 en casa y 555–3226 en el trabajo"; 
+Quotation[11] = "Springfield es un lugar ficticio. Matt Groening afirma que utilizó este nombre porque es uno de los nombres de ciudad más vulgares y porque Springfield era la ciudad más próxima a Portland, Oregón, donde Matt vivió de pequeño"; 
+Quotation[12] = "En el episodio donde le hacen una auditoría a Homer le llaman Homer Jimeno, se entiende que la J en su nombre es de Jimeno, pero después cuando va a averiguar de que es la J en su nombre descubre que la J es de Jay"; 
+Quotation[13] = "El color de la piel de los personajes es siempre: RGB 255 / 217 / 15 "; 
+Quotation[14] = "En la VidaReal™ Homer y Margaret (Marge) son los nombres de los padres de Matt. Los hijos de Matt son Homer y Abe. Sus hermanos, por orden de nacimiento, son Mark, Patty (a quien sigue Matt), Lisa y Maggie, diminutivo cariñoso de Margaret"; 
+Quotation[15] = "El nombre de Bart es un juego de palabras de «brat» (mocoso), aunque más de una vez Matt ha dicho que el personaje está fuertemente inspirado en él mismo (Matt) y su hermano (Mark)"; 
+Quotation[16] = "El apellido Simpson significa literalmente «Hijo de un simplón/inocentón"; 
+Quotation[17] = "En la película The Day of the Locust el protagonista (interpretado por Donald Sutherland) se llama Homer Simpson, y aparte del nombre ambos personajes comparten cierto parecido. No se sabe con certeza si existe alguna conexión entre la película (estrenada en 1975) y la serie, si es pura coincidencia o si simplemente Matt aprovechó esa coincidencia"; 
+Quotation[18] = "El apellido de soltera de la madre de Matt es Wiggum, nombre del jefe de policía de Springfield, mientras que el resto de los personajes secundarios toman sus nombres de calles de Portland"; 
+Quotation[19] = "Según el permiso de conducir de Homer, C40403243, mide 1,76, pesa 108 kilos, tiene ojos azules, es calvo y nació el 12 de mayo de 1956"; 
+Quotation[20] = "El amigo de Bart recibe el nombre del ex–presindete Richard Milhous Nixon, aunque su nombre completo es Milhouse Van Houten y sus padres Kirk y Luann Van Houten, los cuales se divorcian durante la octava temporada, en el episodio «A Milhouse Divided» («Milhouse dividido»)"; 
+Quotation[21] = "La canción que suena en el episodio «Lisa's sax» («El saxo de Lisa») es «Baker Street» de Gerry Rafferty. Puede encontrarse en su álbum «Right Down The Line. The Best of Gerry Rafferty“"; 
+Quotation[22] = "La producción de cada temporada se inicia en diciembre, cuando los guionistas desarrollan unas 16 ideas que se plasman en más o menos 12 guiones. La Primera temporada cada episodio constaba de unos 12.000 dibujos. Los episodios más recientes pueden llegar a necesitar hasta 24.000"; 
+Quotation[23] = "En muchos de los episodios de las primeras temporadas se puede ver que a veces Lisa tiene el color normal de su collar y en otras partes su color cambia por el color del collar de Marge. A veces a Marge le sucede lo mismo con el collar de Lisa"; 
+Quotation[24] = "Según el director y productor de la serie David Silverman, Springfield se encuentra en el estado de North Takoma, a unos 15 kilómetros de Toon Town. En el episodio «Mr. Lisa Goes to Washington» la dirección de los Simpson es Evergreen Terrace, 59, Springfield, TA, donde TA seguramente se refiere a Takoma, mientras que en el episodio «Duffless» la matrícula del coche de Homer muestra las siglas NT, que podrían referirse a North Takoma"; 
+Quotation[25] = "Herman perdió el brazo al sacarlo por la ventanilla del autobús contradiciendo a su profesor"; 
+Quotation[26] = "La familia Simpson fue creada en 15 minutos mientras Matt Groening esperaba en el recibidor de la oficina de James L. Brooks, una historia confirmada por el mismo Groening en una entrevista con Oprah Winfrey."; 
+Quotation[27] = "El Pelo de Bart esta comformado siempre por 9 puntas"; 
+Quotation[28] = "En el episodio “Y con Maggie Tres”, Homero tiene 3 pelos en la cabeza, y se arranca uno cuando se enterá que Marge está embarazada, quedandole los dos habituales. en el mismo episodio muestran como va quedando pelado Homero y es que cada ves que se entera de que Marge esta enbarazada se arranca pelos de su cabeza "; 
+Quotation[29] = "El personaje favorito de los internautas es “Homero” con diferencia, seguido de “Bart” y de “Ralph Wiggun“. “Lisa” es la cuarta, “Maggie” la octava y “Marge” está en el puesto 30"; 
+Quotation[30] = "En el intro de la serie cuando el vendedor pasa a Maggie se ve que cuesta 847,63 Dólares este era el costo de mantencion por mes de un bebe en Estados Unidos"; 
+Quotation[31] = "La loca de los gatos era una niña prodigio, se matriculó en Yale, doctorado y abogacia. Pero acabó consumida en el alcohol y se obsesionó con su gato"; 
+Quotation[32] = "Apu está licenciado en ingeniería y doctor de informática"; 
+Quotation[33] = "Homer viajó a Holanda solo para comer Tulipanes"; 
+Quotation[34] = "Abraham Simpson, el abuelo, ha estado con 5 mujeres en toda su vida, con 3 de ellas tuvo 1 hijo correspondientemente: Homer Jay Simpson, Herb Powell, y Abbie (su hija inglesa)"; 
+Quotation[35] = "Kirk Van Houten trabajaba en la fábrica de galletas Southern Cracker, pero debido a su inexperiencia, la fábrica, hasta el momento en primer lugar, bajó al sexto de la ciudad."; 
+Quotation[36] = "El bajo Intelecto de Homer Simpson no se debe ni a experimetos en el ejército, ni a los constantes golpes de cabeza, electro-shocks, la constante exposición a radicación en la planta nuclear, ni mucho menos al gen “Simpson” en el cromosoma “Y”, si no que segun un capítulo de la serie es por que tiene en el cerebro un lápiz de cera para colorear que el mismo se metió durante su niñez"; 
+Quotation[37] = "La palabra que Homero expresa como queja, ¡Ou! (”D’oh!”, inglés) que la usa típicamente cuando se hace daño, se da cuenta de que ha hecho algo estúpido o malo, o cuando algo malo le ha pasado, no fue invetada por él, si no que la aprendió de su Padre, Abraham Simpson. Esta frase que ha hecho tan famosa se ha agregado al Diccionario Oxford de inglés"; 
+Quotation[38] = "En el episodio 299 dieron la dirección de correo electrónico de Homero, chunkylover53 aol.com. El productor y escritor Matt Groening registró la dirección antes de la emisión del episodio. El contesta desde entonces a los fans que escribieron a esa dirección después del episodio"; 
+
+// Calculamos la longitud el vector (número de frases del stock)
+var Q = Quotation.length; 
+
+// Obtener un número aleatorio entre 1 y la cantidad de frases incluidas 
+//Utilizando la clase Math y el método random(). 
+var whichQuotation=Math.round(Math.random()*(Q-1)); 
+
+// Creamos una función para mostrar la frase 
+function showQuotation() { 
+document.write(Quotation[whichQuotation]);}
